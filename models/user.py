322b831c7d8a009173533f66802f336849863541
Svngoku <marymarty104@gmail.com>
2019-07-17 23:34:15 +0200

    update model user

--- models/user.py
+++ models/user.py
@@ -1,6 +1,6 @@
 from  Crypto.Hash import MD5
 
-def createUser():
+async def createUser():
     psw = MD5.new()
     name = input('Enter your name: ')
     password = input('Enter your password: ')
@@ -11,6 +11,22 @@ def createUser():
         print(user)
     else:
         AssertionError
+
+
+def login_user(username, password):
+    # check entry of values 
+
+    # if check not valid
+        # return a throw
+    # else
+        # execute the query 
+        # close connexion
+
+        # save values in session & store the username inside cookies
+
+        # and return to the dashboard
+    # clean up values 
+    print("DONE ")
 
 createUser()
 
